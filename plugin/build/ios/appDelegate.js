c2ed2aa75120bf90c2ec3dd05e00845edf737f10
Mansfera <83674462+Mansfera@users.noreply.github.com>
2025-04-08 14:43:44 +0300

    Update appDelegate.js
    Remove unnecessary (Connecting...) suffix

--- plugin/build/ios/appDelegate.js
+++ plugin/build/ios/appDelegate.js
@@ -56,7 +56,7 @@ const withIosAppDelegate = (config) => {
 - (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion
 {
     NSString *uuid = payload.dictionaryPayload[@"uuid"];
-    NSString *callerName = [NSString stringWithFormat:@"%@ (Connecting...)", payload.dictionaryPayload[@"callerName"]];
+    NSString *callerName = [NSString stringWithFormat:@"%@", payload.dictionaryPayload[@"callerName"]];
     NSString *handle = payload.dictionaryPayload[@"handle"];
 
     [RNVoipPushNotificationManager addCompletionHandler:uuid completionHandler:completion];
